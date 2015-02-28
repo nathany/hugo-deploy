2ad6b96c2bc771bb0f7d7cf6c3b94da6f5648e0a
Nathan Youngman <git@nathany.com>
2015-02-28 14:11:01 -0700

    CircleCI deployment files.
    I've been working on these for a while with edmontongo.org, fsnotify.org and luanova.org.

---
+++ ci-install-sassc.sh
@@ -0,0 +1,19 @@
+SASS_VERSION=3.1.0
+
+set -x
+set -e
+
+# Install SassC if not already cached or upgrade an old version.
+if [ ! -e $CIRCLE_BUILD_DIR/bin/sassc ] || ! [[ `sassc -v` =~ "sassc: ${SASS_VERSION}" ]]; then
+  export SASS_BUILD_DIR=$HOME/src/github.com/sass
+  mkdir -p $SASS_BUILD_DIR
+  cd $SASS_BUILD_DIR
+
+  git clone --recursive https://github.com/sass/libsass.git -b ${SASS_VERSION}
+  git clone https://github.com/sass/sassc.git -b ${SASS_VERSION}
+
+  cd sassc
+  SASS_LIBSASS_PATH=$SASS_BUILD_DIR/libsass make
+
+  cp $SASS_BUILD_DIR/sassc/bin/sassc $CIRCLE_BUILD_DIR/bin/sassc
+fi
