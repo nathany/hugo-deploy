bff9dd287e0eb27dffc3e4fb06b534f4bc3cc127
Nathan Youngman <git@nathany.com>
2015-02-28 21:51:36 -0700

    watch: no need to invoke sh in this case
    thanks to @cespare for the tip.

--- watch.sh
+++ watch.sh
@@ -3,5 +3,5 @@ SASSC_COMMAND='sassc sass/all.sass static/all.css --style compressed'
 # execute SassC at startup
 eval $SASSC_COMMAND
 # watch Sass and Hugo files during development
-reflex -r '\.(sass|scss)$' -- sh -c "${SASSC_COMMAND}" &
+reflex -r '\.(sass|scss)$' -- ${SASSC_COMMAND} &
 hugo server -D --watch && fg
