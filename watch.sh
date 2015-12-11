3ebcf2a9b1de9aa5eccf1c5c26ccb8f12cb478d2
Nathan Youngman <git@nathany.com>
2015-12-11 09:32:48 -0700

    fix: notify was hanging around after ctrl-c

--- watch.sh
+++ watch.sh
@@ -2,6 +2,15 @@ SASSC_COMMAND='sassc sass/all.sass static/all.css --style compressed'
 
 # execute SassC at startup
 eval $SASSC_COMMAND
+
+# Ctrl-C kills both commands
+trap killgroup SIGINT
+killgroup(){
+  kill 0
+}
+
 # watch Sass and Hugo files during development
-notify -c ${SASSC_COMMAND} sass/** &
+notify -c "${SASSC_COMMAND}" sass/** &
 hugo server -v -D --watch && fg
+
+
