c0fc1e0587a2a227f650a61b9927c5a68be7d801
Nathan Youngman <git@nathany.com>
2017-01-27 20:49:15 -0700

    adjust for hugo tar contents changing

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -7,5 +7,6 @@ set -e
 # Install Hugo if not already cached or upgrade an old version.
 if [ ! -e $CIRCLE_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
   wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
-  tar xvzf ${HUGO_DOWNLOAD} -C $CIRCLE_BUILD_DIR/bin/
+  tar xvzf ${HUGO_DOWNLOAD} "hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64"
+  mv  "hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64" $CIRCLE_BUILD_DIR/bin/hugo
 fi
