2933ae19e5e5c2a9cf082ca2d8e5a2e69a3b93bc
Nathan Youngman <git@nathany.com>
2018-06-06 16:44:26 -0600

    CircleCI 2.0

--- ci-install-s3deploy.sh
+++ ci-install-s3deploy.sh
@@ -1,12 +1,11 @@
-S3DEPLOY_VERSION=2.0.2
 S3DEPLOY_DOWNLOAD=s3deploy_${S3DEPLOY_VERSION}_Linux-64bit.tar.gz
 
 set -x
 set -e
 
 # Install s3deploy if not already cached or upgrade an old version.
-if [ ! -e $CIRCLE_BUILD_DIR/bin/s3deploy ] || ! [[ `s3deploy version` =~ v${S3DEPLOY_VERSION} ]]; then
+if [ ! -e $HOME/bin/s3deploy ] || ! [[ `$HOME/bin/s3deploy -V` =~ ${S3DEPLOY_VERSION} ]]; then
   wget https://github.com/bep/s3deploy/releases/download/v${S3DEPLOY_VERSION}/${S3DEPLOY_DOWNLOAD}
   tar xvzf ${S3DEPLOY_DOWNLOAD} s3deploy
-  mv s3deploy $CIRCLE_BUILD_DIR/bin/s3deploy
+  mv s3deploy $HOME/bin/s3deploy
 fi
